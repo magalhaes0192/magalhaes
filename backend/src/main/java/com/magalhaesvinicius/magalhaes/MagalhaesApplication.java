6b86df6afe91323777792d799482e05493881669
Vinicius <magalhaes01234@gmail.comgit config --global user.email magalhaes01234@gmail.com>
2022-09-06 10:51:02 -0300

    Project created

---
+++ backend/src/main/java/com/magalhaesvinicius/magalhaes/MagalhaesApplication.java
@@ -0,0 +1,13 @@
+package com.magalhaesvinicius.magalhaes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagalhaesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagalhaesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/magalhaesvinicius/magalhaes/MagalhaesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magalhaesvinicius.magalhaes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagalhaesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
